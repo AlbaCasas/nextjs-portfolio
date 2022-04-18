f34adcba49c866d0ea6333918f50910a722b0f45
Alba Casas <albacasasarzua1b@gmail.com>
2022-04-18 18:30:01 +0200

    Fix styles

--- src/components/Tooltip/Tooltip.jsx
+++ src/components/Tooltip/Tooltip.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import { StyledTooltip } from "./styled";
 
 const Tooltip = () => {
-  return <StyledTooltip />;
+  return (
+    <StyledTooltip
+      arrowColor="transparent"
+      backgroundColor="white"
+      textColor="black"
+    />
+  );
 };
 
 export default Tooltip;
--- src/components/Tooltip/styled.js
+++ src/components/Tooltip/styled.js
@@ -2,7 +2,6 @@ import styled from "styled-components";
 import ReactTooltip from "react-tooltip";
 
 export const StyledTooltip = styled(ReactTooltip)`
-  background: white;
   font-size: 18px !important;
   font-family: ${({ theme }) => theme.fonts.body};
   padding: 5px 15px !important;
