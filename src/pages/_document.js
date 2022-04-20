d40a5a70dc0a44a3422fb833649e3e43b9da9246
Alba Casas <albacasasarzua1b@gmail.com>
2022-04-20 17:20:56 +0200

    fix styled components ssr

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,7 +1,8 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
+export default class MyDocument extends Document {
+  render() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -15,6 +16,32 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
-    </Html>
-  );
+    </Html>;
+  }
+
+  static async getInitialProps(ctx) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
 }
